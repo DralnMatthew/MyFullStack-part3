3779f5631830ac023e5ba4f7822bcf1989064bfc
matt <zhanghu.zhao@outlook.com>
2022-05-30 20:21:14 +0800

    part3

--- index.js
+++ index.js
@@ -93,16 +93,18 @@ app.put('/api/persons/:id', (request, response,next) => {
         .catch(error => next(error))
 })
 
-const unknownEndpoint = (req, res) => {
-    res.status(404).send({ error: 'unknown endpoint' })
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
 
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    }
+    else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
     next(error)
 }
