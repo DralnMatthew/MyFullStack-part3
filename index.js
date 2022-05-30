720ec9dbc2b687ce4f0dcc37e763debbd8283e27
matt <zhanghu.zhao@outlook.com>
2022-05-30 18:36:24 +0800

    part3

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/', (request, response) => {
 
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
-        response.json(persons.map(person => person.toJSON()))
+        response.json(persons)
     })
 })
 
@@ -44,19 +44,24 @@ app.get('/info', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+app.get('/api/persons/:id', (request, response,next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            response.json(person)
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    Person.findByIdAndRemove(request.params.id).then(() =>{
-        response.status(204).end()
-    })
+app.delete('/api/persons/:id', (request, response,next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result =>{
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response,next) => {
     const body = request.body
     if (!body.name || !body.number) {
         return response.status(404).json({
@@ -68,7 +73,38 @@ app.post('/api/persons', (request, response) => {
         number: body.number
     })
 
-    person.save().then(person => {
-        response.json(person.toJSON())
-    })
+    person.save()
+        .then(person => {
+            response.json(person)
+        })
+        .catch(error => next(error))
 })
+
+app.put('/api/persons/:id', (request, response,next) => {
+    const body = request.body
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+app.use(errorHandler)
+
