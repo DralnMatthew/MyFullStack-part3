ae62493bee1bd656a2a270c4a0ac0cb1cad3432a
matt <zhanghu.zhao@outlook.com>
2022-07-31 10:52:13 +0800

    part3-final

--- index.js
+++ index.js
@@ -15,8 +15,17 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
 
-morgan.token('data', (req) => JSON.stringify(req.body))
-app.use(morgan(':method :url :status  :res[content-length] - :response-time ms :data '))
+morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
+morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.data(req, res)
+  ].join(' ')
+})
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
